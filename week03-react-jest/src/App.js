7fbc709e3b81bf7a702a639eb82898eabd5678ed
Matthew Home <matthew_wilkes7@hotmail.com>
2018-04-20 15:00:47 -0700

    Week 3 Unit Tests Address

--- week03-react-jest/src/App.js
+++ week03-react-jest/src/App.js
@@ -3,16 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-    getFile = () => {
+    getFileName = () => {
         console.log('getFile called.');
         this.setState({file: 'url-file.js'})
+    };
+    setAddress = () => {
+        console.log('setAddress called.');
+        this.setState({
+            firstName: 'Patty',
+            lastName: 'Murray',
+            street: '915 2nd Ave',
+            city: 'Seattle',
+            state: 'Washington',
+            zipCode: '98174'
+        })
     };
     constructor() {
         super();
         this.state = {
-            file: 'unknown'
+            file: 'unknown',
+            firstName: 'unknown',
+            lastName: 'unknown',
+            street: 'unknown',
+            city: 'unknown',
+            state: 'unknown',
+            zipCode: 'unknown'
         }
     }
+
     render() {
         return (
             <div className="App">
@@ -20,8 +38,15 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo"/>
                     <h2 className="App-title">Welcome to React</h2>
                 </header>
-                <button id='getFile' onClick={this.getFile}>Get File</button>
+                <button id='getFile' onClick={this.getFileName}>Get File</button>
+                <button id="setAddress" onClick={this.setAddress}>Set Address</button>
                 <p className="App-intro">File: {this.state.file}</p>
+                <p className="App-intro">First Name: {this.state.firstName}</p>
+                <p className="App-intro">Last Name: {this.state.lastName}</p>
+                <p className="App-intro">Street: {this.state.street}</p>
+                <p className="App-intro">City: {this.state.city}</p>
+                <p className="App-intro">State: {this.state.state}</p>
+                <p className="App-intro">Zip Code: {this.state.zipCode}</p>
 
 
             </div>
--- week03-react-jest/src/App.test.js
+++ week03-react-jest/src/App.test.js
@@ -32,4 +32,82 @@ describe('jest test', function() {
         expect(wrapper.contains(nineSign)).toBe(true);
     });
 
+    it('renders and displays the default first name', () => {
+        const wrapper = shallow(<App />);
+        const welcome = <p className="App-intro">First Name: unknown</p>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders state of firstname paragraph after button click', () => {
+        const wrapper = shallow(<App />);
+        const firstName = <p className="App-intro">First Name: Patty</p>;
+        wrapper.find('#setAddress').simulate('click');
+        expect(wrapper.contains(firstName)).toBe(true);
+    });
+
+    it('renders and displays the default last name', () => {
+        const wrapper = shallow(<App />);
+        const welcome = <p className="App-intro">Last Name: unknown</p>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders state of lastname paragraph after button click', () => {
+        const wrapper = shallow(<App />);
+        const firstName = <p className="App-intro">Last Name: Murray</p>;
+        wrapper.find('#setAddress').simulate('click');
+        expect(wrapper.contains(firstName)).toBe(true);
+    });
+
+    it('renders and displays the default street', () => {
+        const wrapper = shallow(<App />);
+        const welcome = <p className="App-intro">Street: unknown</p>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders state of street paragraph after button click', () => {
+        const wrapper = shallow(<App />);
+        const firstName = <p className="App-intro">Street: 915 2nd Ave</p>;
+        wrapper.find('#setAddress').simulate('click');
+        expect(wrapper.contains(firstName)).toBe(true);
+    });
+
+    it('renders and displays the default city', () => {
+        const wrapper = shallow(<App />);
+        const welcome = <p className="App-intro">City: unknown</p>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders state of city paragraph after button click', () => {
+        const wrapper = shallow(<App />);
+        const firstName = <p className="App-intro">City: Seattle</p>;
+        wrapper.find('#setAddress').simulate('click');
+        expect(wrapper.contains(firstName)).toBe(true);
+    });
+
+    it('renders and displays the default state', () => {
+        const wrapper = shallow(<App />);
+        const welcome = <p className="App-intro">State: unknown</p>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders state of state paragraph after button click', () => {
+        const wrapper = shallow(<App />);
+        const firstName = <p className="App-intro">State: Washington</p>;
+        wrapper.find('#setAddress').simulate('click');
+        expect(wrapper.contains(firstName)).toBe(true);
+    });
+
+    it('renders and displays the default Zip code', () => {
+        const wrapper = shallow(<App />);
+        const welcome = <p className="App-intro">Zip Code: unknown</p>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
+
+    it('renders state of zip code paragraph after button click', () => {
+        const wrapper = shallow(<App />);
+        const firstName = <p className="App-intro">Zip Code: 98174</p>;
+        wrapper.find('#setAddress').simulate('click');
+        expect(wrapper.contains(firstName)).toBe(true);
+    });
+
 });
