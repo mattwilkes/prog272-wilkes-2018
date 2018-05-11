ead04ceae2b39d02fc09a54992e8b59ccd55921f
Matthew Home <matthew_wilkes7@hotmail.com>
2018-05-11 13:11:19 -0700

    ReactAddressMenu

--- week03-react-jest/src/Components/App.js
+++ week03-react-jest/src/Components/App.js
@@ -1,18 +1,22 @@
 import React, {Component} from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
 import '../App.css';
 import Address from './Address'
 import Headers from './Headers';
 import GetFile from './GetFile';
+import '../menu.css';
 
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <Headers/>
-                <GetFile/>
-                <Address/>
-            </div>
+            <BrowserRouter>
+                <div className="App">
+                    <Headers />
+                    <Route exact path="/" component={Address}/>
+                    <Route path="/get-file" component={GetFile}/>
+                </div>
+            </BrowserRouter>
         );
     }
 }
--- week03-react-jest/src/Components/Headers.js
+++ week03-react-jest/src/Components/Headers.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
+import { Link } from 'react-router-dom';
+
 
 class Headers extends Component {
+
 
     render() {
         return(
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo"/>
             <h2 className="App-title">Welcome to React</h2>
+            <div>
+                <div className="App">
+                    <ul>
+                        <li><Link to="/">Address</Link></li>
+                        <li><Link to="/get-file">Get File</Link></li>
+                    </ul>
+                </div>
+            </div>
         </header>
+
+
         );
+
     }
 }
 export default Headers
